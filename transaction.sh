1c6a4c22cbab19b52b884800272751c115a6a1ee
Nitish kumar mishra <65211739+mrnitishmishra@users.noreply.github.com>
2024-11-28 22:49:26 +0530

    Update transaction.sh

--- transaction.sh
+++ transaction.sh
@@ -4,6 +4,23 @@
 account_number=$1
 transaction_type=$2
 amount=$3
+log_file="transaction.sh"
+
+if [["$transaction_type" != "deposite" && "$transaction_type" != "withdrawal" ]]; then
+    echo "Error: Transaction type must be either 'deposite' or 'withdrawl'."
+    exit 1
+fi
+
+if ! [[ "$amount" =~ ^[0-9]+([.][0-9]+)?$ ]]; then
+    echo "Error: Amount must be a positive number."
+    exit 2
+fi
+
+timestamp=$(date + "%Y-%m-%d %H:%M:%S")
+
+echo "$timestamp | Account: $account_number | Type: $transaction_type | Amount: $amount" >>$log_file"
+
+echo "transaction logged successfully in $log_file."
 
 # Get the current timestamp
 timestamp= #todo
