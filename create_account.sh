76c3fb60f2d672f0777e7be14a87e2b1a003649e
Nitish kumar mishra <65211739+mrnitishmishra@users.noreply.github.com>
2024-11-28 22:44:23 +0530

    Add input parameters create_account.sh

--- create_account.sh
+++ create_account.sh
@@ -3,6 +3,27 @@
 # Input parameters: username and account number
 username=$1
 account_number=$2
+transaction_type=$3
+amount=$4
+log_file="transaction.sh"
+
+if [["$transaction_type" != "deposite" && "$transaction_type" != "withdrawal" ]]; then
+    echo "Error: Transaction type must be either 'deposite' or 'withdrawl'."
+    exit 1
+fi
+
+if ! [[ "$amount" =~ ^[0-9]+([.][0-9]+)?$ ]]; then
+    echo "Error: Amount must be a positive number."
+    exit 2
+fi
+
+timestamp=$(date + "%Y-%m-%d %H:%M:%S")
+
+echo "$timestamp | Account: $account_number | Type: $transaction_type | Amount: $amount" >>$log_file"
+
+echo "transaction logged successfully in $log_file."
+
+
 
 # Default balance for new accounts
 default_balance= #todo
